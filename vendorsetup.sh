c0492fd6883d38227087015bd4180fd643d3baf7
askthekaif <209830895+askthekaif@users.noreply.github.com>
2025-06-27 06:37:23 +0530

    sky: Updated vendor, kernel, hardware xiaomi

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,19 +4,19 @@ echo "Setting up repositories for Redmi 12 5G / Poco M6 Pro 5G (sky)..."
 
 # Clone the kernel source
 echo "Cloning kernel repository..."
-git clone https://github.com/askthekaif/android_xiaomi_sky_kernel.git device/xiaomi/sky-kernel
+git clone https://github.com/askthekaif/android_xiaomi_sky_kernel.git -b 15.0-AtomX device/xiaomi/sky-kernel
 
 # Clone the vendor source
 echo "Cloning vendor repository..."
-git clone https://github.com/askthekaif/android_vendor_xiaomi_sky.git vendor/xiaomi/sky
+git clone https://github.com/askthekaif/android_vendor_xiaomi_sky.git -b 16.0 vendor/xiaomi/sky
 
 # Clone the dolby repository
 echo "Cloning dolby hardware repository..."
-git clone https://github.com/askthekaif/android_hardware_dolby hardware/dolby
+git clone https://github.com/askthekaif/android_hardware_dolby.git hardware/dolby
 
 # Clone the hardware repository
 echo "Cloning hardware repository..."
-git clone https://github.com/PixelOS-AOSP/hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/Evolution-X-Devices/hardware_xiaomi.git -b bka hardware/xiaomi
 
 # Clone the agm repository
 echo "cloning agm repository..."
