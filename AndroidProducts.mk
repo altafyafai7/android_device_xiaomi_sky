4295bc553a85d1f853348a050c27e6ab6b72bf50
Altaf Yafai <49188943+AltafYafai@users.noreply.github.com>
2025-07-06 15:23:01 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_sky.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sky-bp1a-eng \
-    lineage_sky-bp1a-userdebug \
-    lineage_sky-bp1a-user
+    lineage_sky-bp2a-eng \
+    lineage_sky-bp2a-userdebug \
+    lineage_sky-bp2a-user
